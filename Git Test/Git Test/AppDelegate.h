a5639de263bc87e6089f8b449d65b23df343fb4b
Wankil Jung <>
2018-08-14 09:26:17 +0900

    first add

---
+++ Git Test/Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Wankil Jung on 2018. 8. 14..
+//  Copyright © 2018년 Wankil Jung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Wankil Jung on 2018. 8. 14..
+//  Copyright © 2018년 Wankil Jung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
